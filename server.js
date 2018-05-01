1a432e73d21cd492f123beb8915e4d70b0589443
Louis Gaillet <louisgaillet@hotmail.fr>
2018-05-01 17:21:42 +0200

    Maj routes server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8080;
 const server = express();
 server.use(express.static(__dirname));
 
-server.get("/bonjour", (req, res) => {
+server.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname,"index.html"))
 })
 
