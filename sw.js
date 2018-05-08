1e9af955f9591e0ee2af46230fb6d1388c06b2f1
Louis Gaillet <louisgaillet@hotmail.fr>
2018-05-08 12:55:17 +0200

    [PWA] Ajout du manifest.json

---
+++ sw.js
@@ -0,0 +1,43 @@
+
+const cacheName = 'React_sound' + '1.1.1';
+
+self.addEventListener('install', (evt) => {
+    console.log(`sw installé à ${new Date().toLocaleTimeString()}`);
+    const cachePromise = caches.open(cacheName).then(cache => {
+        return cache.addAll([
+            'index.html',
+            './bundle.js',
+            './style/style.css'
+        ])
+            .then(console.log('cache initialisé'))
+            .catch(console.err);
+    });
+
+    evt.waitUntil(cachePromise);
+
+});
+
+self.addEventListener('activate', (evt) => {
+    console.log(`sw activé à ${new Date().toLocaleTimeString()}`);
+    let cacheCleanedPromise = caches.keys().then(keys => {
+        keys.forEach(key => {
+            if (key !== cacheName) {
+                return caches.delete(key);
+            }
+        });
+    });
+    evt.waitUntil(cacheCleanedPromise);
+});
+
+self.addEventListener('fetch', (evt) => {
+
+
+    // always serving css from the cache
+    if(evt.request.url.includes('css')) {
+        caches.open(cacheName).then(cache => {
+            cache.match(evt.request);
+        })
+    } else {
+        evt.respondWith(fetch(evt.request));
+    } 
+});
