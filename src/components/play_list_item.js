c8142ce29ec7c68cb7da96a992a23b7c7f8786cd
Louis Gaillet <louisgaillet@hotmail.fr>
2018-06-07 07:55:03 +0200

    Ajout menu context

--- src/components/play_list_item.js
+++ src/components/play_list_item.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import {Link } from 'react-router-dom'
+import { ContextMenu, MenuItem, ContextMenuTrigger } from "react-contextmenu";
 import * as routes from '../config/routes'
 import axios from 'axios'
+
 
 class PlayListItem extends Component {
   constructor(props) {
     super(props);
     this.state = {  };
+  }
+
+  ref = contextTrigger => {
+    this.contextTrigger = contextTrigger
+}
+
+  toggleMenu = e => {
+    if(this.contextTrigger) {
+        this.contextTrigger.handleContextClick(e);
+    }
+  };
+
+  handleClick(){
+    
   }
 
   download(){
@@ -44,17 +60,39 @@ class PlayListItem extends Component {
       }
     }
   }
-  render() {
+
+  renderMenuOptions(){
     return (
       <div>
+        <ContextMenuTrigger ref={(c) => this.contextTrigger = c}>
+          <span onClick={this.toggleMenu} className="text-white mr-4 pl-2 pr-2 open-menu-trigger"><i className="fas fa-ellipsis-v"></i></span>
+        </ContextMenuTrigger>
+
+        <ContextMenu>
+          <MenuItem data={{foo: 'bar'}} onClick={this.handleClick}>
+            Supprimer
+          </MenuItem>
+          <MenuItem data={{foo: 'bar'}} onClick={this.handleClick}>
+            Synchroniser
+          </MenuItem>
+      </ContextMenu>
+      </div>
+    )
+  }
+  
+  render() {
+    return (
+      <div className="item-playlist">
         {/* <span onClick={ (e) => this.download()}> Download</span> */}
+        {this.renderMenuOptions()}
       {this.props.style ?
-        <li className="list-group-item bg-transparent d-flex align-items-center  mb-1">
-        <div className="thumbnail_playlist mr-3">
-          {this.renderThumbnailPlaylist()}
-        </div>
-        
-        <Link to={`${routes.DETAILPLAYLIST}/${this.props.id}`}>{this.props.name}</Link></li>
+        <Link to={`${routes.DETAILPLAYLIST}/${this.props.id}`}>{this.props.name}
+          <li className="list-group-item bg-transparent d-flex align-items-center  mb-1">
+            <div className="thumbnail_playlist mr-3">
+              {this.renderThumbnailPlaylist()}
+            </div>
+          </li>
+        </Link>
         : <p onClick={(e) => this.props.callback(this.props.id)}>{this.props.name}</p>
       }
       </div>
